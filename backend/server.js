5cf9f75e08ddf7683e733410cf12a8a6e9c7977b
Sean Winnik <winnik.sean@gmail.com>
2023-08-27 15:14:43 -0700

    node config

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, "../dist")));
 // app.get('/api/data', (req, res) => {
 //   // Handle API request
 // });
+//
 
 // Serve React app for all other routes
 app.get("*", (req, res) => {
