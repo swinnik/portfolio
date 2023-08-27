cb87477ec063faed05624b91dcc31a33b9ac4530
Sean Winnik <winnik.sean@gmail.com>
2023-08-27 15:11:54 -0700

    initial node commit

--- src/components/OpenAi.jsx
+++ src/components/OpenAi.jsx
@@ -7,7 +7,7 @@ const OpenAIChatComponent = () => {
   const [inputText, setInputText] = useState("");
   const [responseText, setResponseText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const OPENAI_API_KEY = "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";
+  const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
   const handleButtonClick = () => {
     setIsLoading(true); // Set loading state
