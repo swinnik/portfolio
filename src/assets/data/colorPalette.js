5e19ce28769b2edfa11dad42013187eefaf48a81
Sean Winnik <winnik.sean@gmail.com>
2023-08-29 12:39:04 -0700

    full screen

--- src/assets/data/colorPalette.js
+++ src/assets/data/colorPalette.js
@@ -3,6 +3,7 @@ const colorPalette = {
   bio: "#279EFF",
   work: "#40F8FF",
   text1: "#D5FFD0",
+  background: "#0C356A",
 };
 
 export default colorPalette;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ const styles = {
     top: 0,
     left: 0,
     width: "100%",
+    height: "100%",
     color: colorPalette.text1,
-    backgroundColor: "red",
   },
 };
