618006a3159356f21bc3ffb0043f42006ee0418a
Sean Winnik <winnik.sean@gmail.com>
2023-08-30 14:15:37 -0700

    PORT || 8080

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
